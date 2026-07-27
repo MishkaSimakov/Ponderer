25973fa12e2c8e8658b9ffd278f8c2e5a350bb54
MishkaSimakov <msimakov661@gmail.com>
2026-07-27 20:44:40 +0300

    init

---
+++ server/csv_logger.py
@@ -0,0 +1,14 @@
+import csv
+
+
+class CsvLogger:
+    def __init__(self, path, columns):
+        self.file = open(path, "w", newline="")
+        self.writer = csv.writer(self.file)
+        self.writer.writerow(columns)
+
+    def log(self, *values):
+        self.writer.writerow(values)
+
+    def close(self):
+        self.file.close()
---
+++ server/main.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+from ev3dev2.motor import Motor, OUTPUT_A, OUTPUT_B, SpeedPercent
+from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3
+from ev3dev2.sensor.lego import ColorSensor, UltrasonicSensor
+
+from csv_logger import CsvLogger
+
+import time
+
+frequency = 20  # 20 Hz
+period = 1 / frequency
+
+overruns = 0
+start_t = time.monotonic()
+next_t = start_t
+
+distance_sensor = UltrasonicSensor(INPUT_1)
+left_color_sensor = ColorSensor(INPUT_3)
+right_color_sensor = ColorSensor(INPUT_2)
+
+left_motor = Motor(OUTPUT_A)
+right_motor = Motor(OUTPUT_B)
+
+logger = CsvLogger("sensors.csv", ["t", "distance", "left_color", "right_color", "left_position", "right_position"])
+
+for i in range(500):
+    next_t += period
+
+    # read sensors
+    distance = distance_sensor.distance_centimeters_continuous
+    left_color = left_color_sensor.reflected_light_intensity
+    right_color = right_color_sensor.reflected_light_intensity
+    left_position = left_motor.position
+    right_position = right_motor.position
+
+    if i == 250:
+        left_motor.run_forever(speed_sp=1560)
+
+    logger.log(time.monotonic() - start_t, distance, left_color, right_color, left_position, right_position)
+
+    lag = next_t - time.monotonic()
+    if lag < 0:
+        overruns += 1
+        next_t = time.monotonic()
+    else:
+        time.sleep(lag)
+
+logger.close()
+left_motor.stop()
+
+with open("main.log", "a") as f:
+    f.write("overruns: " + str(overruns) + "\n")
